9788ba368590a3a57aab7375f79d7306fce88239
DESKTOP-690PG9R\average <bayogjayr@gmail.com>
2024-08-21 16:04:00 +0800

    Added excercises

---
+++ Excercises/ExcerciseOne/Program.cs
@@ -0,0 +1,14 @@
+﻿while (true)
+{
+    Console.Write("Input word (x to exit): ");
+    string word = Console.ReadLine()!;
+    if (word == "x") break;
+
+    string reverse = String.Join("", word!.ToCharArray().Reverse());
+
+    var isPalindrome = string.Compare(word, reverse, StringComparison.OrdinalIgnoreCase);
+
+    if (isPalindrome == 0)
+        Console.WriteLine("String is palindrome");
+    else Console.WriteLine("String is not palindrome");
+}
---
+++ Excercises/ExcerciseTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿
+while (true)
+{
+    Console.Write("Enter number (0 to exit): ");
+    int n = int.Parse(Console.ReadLine()!);
+
+    if (n == 0) break;
+    if (n == 1) Console.WriteLine("1 is neither prime nor composite.");
+    else if (IsPrime(n))
+        Console.WriteLine("Number is prime");
+    else Console.WriteLine("Number is not prime");
+}
+
+bool IsPrime(int number)
+{
+    if (number == 1) return false;
+    if (number == 2) return true;
+
+    int limit = (int)Math.Ceiling(Math.Sqrt(number));
+
+    for (int i = 2; i <= limit; i++)
+    {
+        if (number % i == 0) return false;
+    }
+
+    return true;
+}
